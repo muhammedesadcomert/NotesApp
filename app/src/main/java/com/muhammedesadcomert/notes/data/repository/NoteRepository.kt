557c77e1670f1a8f71c45238089533ff319c3244
Muhammed Esad Cömert <esadcmrt@gmail.com>
2022-08-27 18:01:55 +0300

    Add NotesRepository

---
+++ app/src/main/java/com/muhammedesadcomert/notes/data/repository/NoteRepository.kt
@@ -0,0 +1,17 @@
+package com.muhammedesadcomert.notes.data.repository
+
+import com.muhammedesadcomert.notes.data.local.NoteDao
+import com.muhammedesadcomert.notes.ui.note.model.Note
+import javax.inject.Inject
+
+class NoteRepository @Inject constructor(private val noteDao: NoteDao) {
+    suspend fun insert(note: Note) = noteDao.insert(note)
+
+    suspend fun update(note: Note) = noteDao.update(note)
+
+    suspend fun delete(note: Note) = noteDao.delete(note)
+
+    fun getNote(id: Int): Note = noteDao.getNote(id)
+
+    fun getNotes(): List<Note> = noteDao.getNotes()
+}
